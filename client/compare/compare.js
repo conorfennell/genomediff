78b501c51218464e6b02343c94343298337f2a49
Conor Fennell <fennell.conor@gmail.com>
2014-05-20 00:19:22 -0700

    Created table for risks

--- client/compare/compare.js
+++ client/compare/compare.js
@@ -13,8 +13,36 @@ angular.module('myApp.main.compare', ['ui.router', 'ngCookies'])
   $scope.user1 = {};
   $scope.user2 = {};
 
-  getUserData($cookies.token1, $http, $scope.user2);
-  getUserData($cookies.token2, $http, $scope.user1);
+  $scope.combine = function($user1, $user2) {
+    $scope.combined = {};
+    $scope.combined.firstUser  = $user1.name;
+    $scope.combined.SecondUser = $user2.name;
+    $scope.combined.risks = [];
+    var risk;
+
+    for (var i = 0; i < $user1.risks.length; i++) {
+      var firstWins = 'Draw';
+      if(+$user1.risks[i].risk > +$user2.risks[i].risk) {
+        firstWins = 'Lose';
+      } else if(+$user1.risks[i].risk < +$user2.risks[i].risk) {
+        firstWins = 'Win';
+      }
+
+      risk = {
+        description    : $user1.risks[i].description,
+        firstRisk      : +$user1.risks[i].risk,
+        secondRisk     : +$user2.risks[i].risk,
+        populationRisk : +$user1.risks[i].population_risk,
+        firstWins      : firstWins
+      };
+      $scope.combined.risks.push(risk);
+    }
+
+    console.log($user1.name);
+  };
+
+  getUserData($cookies.token1, $http, $scope.user1);
+  getUserData($cookies.token2, $http, $scope.user2);
 });
 
 var endPoints = {
@@ -39,7 +67,14 @@ var getUserData = function(token, $http, $user){
 
       $http.get(endPoints.risks + '?token=' + token + '&profileId=' + profileId  )
         .then(function(result){
-          $user.risks = result.data.risks;
+          $user.risks = result.data.risks.sort(function(a,b){
+            if (a.description > b.description){
+              return -1;
+            } else if (a.description < b.description) {
+              return 1;
+            }
+            return 0;
+          });
         });
 
       $http.get(endPoints.carriers + '?token=' + token + '&profileId=' + profileId  )
